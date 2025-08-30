c809458d90e83d5d5792309a9e0794fe6568cf05
colin <sadowitzc@gmail.com>
2025-08-30 15:06:04 -0400

    remove git install on docker file, started README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Service Endpoint
+## About
+Connection router; route incoming player traffic to desired location. <br>
+Ex:
+- service-endpoint -> private-lobby
+- service-endpoint -> match-queuer
+
+## Installation
+Docker compose file
+## Usage
--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import socket
+import threading
+
+connections = {}
 
 def main():
-    pass
+    # Change to environment variables?
+    address = ("0.0.0.0", 8008)
+    server = socker.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server.bind(address)
+    server.listen(1)
+
+    while True:
+        connection, address = server.accept()
+
+        connections[connection] = None
+
+        client_thread = threading.Thread(target=client_router, args=(connection,))
+        client_thread.start()
+
+
+def client_router(conn):
+    match (connections[conn]):
+        case None:
+            pass
+        case "Private Lobby":
+            pass
 
 if __name__ == "__main__":
     main()
